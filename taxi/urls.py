db00bdb9dca2d0f1477b06a8031a7fccf81e182d
Andrey Bilenko <olimp_72@ukr.net>
2026-03-03 00:43:48 +0200

    Fix_Bags_Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import index
+
+from taxi.views import index
 
 app_name = "taxi"
 
--- taxi/views.py
+++ taxi/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Driver, Manufacturer, Car
+
+from taxi.models import Driver, Manufacturer, Car
 
 
 def index(request):
